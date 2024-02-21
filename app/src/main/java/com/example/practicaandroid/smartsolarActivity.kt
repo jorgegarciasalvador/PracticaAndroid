1366264b7619d5262b040fd67ba3cfb3dc5ba6e2
jorgegarciasalvador <160707894+jorgegarciasalvador@users.noreply.github.com>
2024-02-21 07:45:56 +0100

    Initial commit

---
+++ app/src/main/java/com/example/practicaandroid/smartsolarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.practicaandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class smartsolarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_smartsolar)
+    }
+}
